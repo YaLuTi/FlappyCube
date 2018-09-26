a9162737613832baa6ee045faad241e10f36d393
DESKTOP-AHQ5MNJ\bigpo <bigpong88508@gmail.com>
2018-09-26 21:33:31 +0800

    Why this world so hard?

---
+++ Assets/Scripts/System/IState.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public abstract class IState
+    {
+        private StateController m_StateController;
+        protected StateController Controller
+        {
+            get
+            {
+                return m_StateController;
+            }
+        }
+        private bool m_bAtStateBegin = true;
+        public bool AtStateBegin
+        {
+            get
+            {
+                return m_bAtStateBegin;
+            }
+        }
+
+        public void SetProperty(StateController oController)
+        {
+            m_StateController = oController;
+        }
+
+        public void TouchStateBegin()
+        {
+            m_bAtStateBegin = false;
+        }
+
+        protected void TransTo(IState oState)
+        {
+            m_StateController.TransTo(oState);
+        }
+
+        public virtual void StateBegin() { }
+        public virtual void StateUpdate() { }
+        public virtual void StateEnd() { }
+
+        public override string ToString()
+        {
+            return string.Format("<IState>" + this.GetType().Name);
+        }
+    }
